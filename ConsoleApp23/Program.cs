5b89454dcc4344dddccee0e7a03be8002b46e056
BushevAndrey <chhufudud@gmail.com>
2023-12-22 19:39:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S = 1;
+            for (double i = 1; i <= 10; i++)
+            {
+                double p = 1;
+                for (double j = 1; j <=5; j++)
+                {
+                    p*=(Abs(Sin(i)*Cos(j)))/(i+j);
+                }
+                S *= p;
+            }
+            Console.WriteLine(S);
+        }
+    }
+}
